0d55c944f66e56e79b5b08fb464db432292d147c
Paresh-95 <Pareshbhamare95@gmail.com>
2024-10-14 20:23:27 +0530

    Ready for deployment

--- api/index.js
+++ api/index.js
@@ -1,29 +1,22 @@
-const dbConnect = require("./config/db");
-const TaskRoutes = require("./routes/TaskRoutes");
+const dbConnect = require("../config/db");
+const TaskRoutes = require("../routes/TaskRoutes");
 require("dotenv").config();
 const http = require('http'); 
 const express = require("express");
 const cors = require("cors")
-const { createWebSocketServer } = require('./websocket');
-
-const app = express();
+const { createWebSocketServer } = require('../websocket');
 
 const PORT = process.env.PORT || 4000;
 
-const corsOptions = {
-    origin: process.env.FRONTEND_URL, 
-    methods: 'GET,POST', 
-  };
-  
-app.use(cors(corsOptions));
-
-
-app.use(express.json());
-
+const app = express();
 
 dbConnect();
 
-
+const corsOptions = {
+    origin: ['http://localhost:3000', process.env.FRONTEND_URL]
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use("/api/v1", TaskRoutes);
 
 
